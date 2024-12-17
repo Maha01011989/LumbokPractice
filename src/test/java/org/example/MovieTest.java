9b83603b4978dde504dc5ba5ca81b59becd0ac49
MahalakshmiTEG <mahalakshmi.g@transportexchangegroup.com>
2024-12-17 12:27:47 +0530

    sample

---
+++ src/test/java/org/example/MovieTest.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MovieTest {
+
+    @
+
+    @Test
+    public void testGetId() {
+    }
+
+    @Test
+    public void testSetId() {
+    }
+
+    @Test
+    public void testGetTitle() {
+    }
+
+    @Test
+    public void testSetTitle() {
+    }
+
+    @Test
+    public void testGetDesc() {
+    }
+
+    @Test
+    public void testSetDesc() {
+    }
+
+    @Test
+    public void testGetCountry() {
+    }
+
+    @Test
+    public void testSetCountry() {
+    }
+
+    @Test
+    public void testGetRating() {
+    }
+
+    @Test
+    public void testSetRating() {
+    }
+
+    @Test
+    public void testGetSite() {
+    }
+
+    @Test
+    public void testSetSite() {
+    }
+
+    @Test
+    public void testGetLanguage() {
+    }
+
+    @Test
+    public void testSetLanguage() {
+    }
+}

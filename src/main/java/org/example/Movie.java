e9bb9e5acc86c38d075e423a0229ac069a094da0
MahalakshmiTEG <mahalakshmi.g@transportexchangegroup.com>
2024-12-18 18:20:49 +0530

    lumbok no arg construtor and all arg constructor

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.net.URL;
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
     private Long id;
@@ -15,62 +19,5 @@ public class Movie {
     private int rating;
     private URL site;
     private String language;
-
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getDesc() {
-//        return desc;
-//    }
-//
-//    public void setDesc(String desc) {
-//        this.desc = desc;
-//    }
-//
-//    public String getCountry() {
-//        return country;
-//    }
-//
-//    public void setCountry(String country) {
-//        this.country = country;
-//    }
-//
-//    public int getRating() {
-//        return rating;
-//    }
-//
-//    public void setRating(int rating) {
-//        this.rating = rating;
-//    }
-//
-//    public URL getSite() {
-//        return site;
-//    }
-//
-//    public void setSite(URL site) {
-//        this.site = site;
-//    }
-//
-//    public String getLanguage() {
-//        return language;
-//    }
-//
-//    public void setLanguage(String language) {
-//        this.language = language;
-//    }
-
 
 }
